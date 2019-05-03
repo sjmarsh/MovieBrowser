4af79913845d83c716a09d4acacd1447838d6cbd
sjmarsh <sjmarsh31@gmail.com>
2019-05-03 16:14:51 +1000

    Added play movie functionality

---
+++ MovieBrowser/wwwroot/app/play.js
@@ -0,0 +1,15 @@
+﻿var app = new Vue({
+    el: '#app',
+    data() {
+        return {
+            title: null,
+            moviePath: null,
+            status: null
+        }
+    },
+    mounted() {
+        var params = new URLSearchParams(window.location.search);
+        this.title = params.get('title');
+        this.moviePath = '/api/play/?title=' + this.title;
+    }
+})
